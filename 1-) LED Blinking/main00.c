e9b68c41c7d6584f083ec8427768ff63951277e6
Kerim Turak <68472927+kerimturak@users.noreply.github.com>
2021-07-14 02:30:58 +0300

    Update main00.c

--- 1-) LED Blinking/main00.c
+++ 1-) LED Blinking/main00.c
@@ -1,34 +1,34 @@
 #include "TM4C123GH6PM.h"
 /* 
-	 LED Blinking
-	 Kerim Turak tarafindan yazilmistir 
-	 Tüm haklari saklidir
+	 LED Blinking ( LED YakÄ±p SÃ¶ndÃ¼rme ) 
+	 Kerim Turak tarafindan yazilmistir ( Written by Kerim Turak )
+	 TÃ¼m haklari saklidir ( All rights reserved )
 	 kerimturak@hotmail.com
 	 10.07.2021
 	 */	 
-/* GPIO pinlerini kullanabilmemiz için öncelikle ilgili GPIO pinlerin 
+/* GPIO pinlerini kullanabilmemiz iÃ§in Ã¶ncelikle ilgili GPIO pinlerin 
 	 baslatilmasi yani ayarlanmasi(yapilandirilmasi) gerekmektedir.*/
 	 
-/* Kullanilacak portlar için yapilandirma adimlari asagidaki gibidir
+/* Kullanilacak portlar iÃ§in yapilandirma adimlari asagidaki gibidir
 
-	1-Port veya çevrebirimin clock'un aktif edilmesi
-	2-Port'un kilidinin açilmasi(unlock) eger kilitliyse. ***(Kilitli port pinleri için geçerli)
+	1-Port veya Ã§evrebirimin clock'un aktif edilmesi
+	2-Port'un kilidinin aÃ§ilmasi(unlock) eger kilitliyse. ***(Kilitli port pinleri iÃ§in geÃ§erli)
 	3-Eger digital fonksiyon kullanilacaksa analog islevselligin deaktif edilmesi(disable) ***(Default olarak 0 yani deaktif) 
-	4-Port control registerinin yapilandirilmasi (Portun GPIO olarak kullanilmasi için) ***(Default olarak 0 yani GPIO olarak ayarlanmistir portlar)
+	4-Port control registerinin yapilandirilmasi (Portun GPIO olarak kullanilmasi iÃ§in) ***(Default olarak 0 yani GPIO olarak ayarlanmistir portlar)
 	5-Direction registerinin yapilandirilmasi(Portun pini input mu olacak output mu)
 	6-DEN(digital enable) registerin yapilandirilmasi ( digital olarak kullanilacaksa eger)
 	7-PUR(pull-up registerin ayarlanmasi) yada PDR(pull-down) eger gerekliyse tabi
 	
-	Yukaridaki adimlardan bazilarinin default degerleri dolayisiyla ayarlanmasina ihtiyaç yoktur.
-	eger portun basit bir digital fonksiyon özelligi kullanilmak istenirse asasgidaki adimlarin takip edilmesi yeterlidir.
+	Yukaridaki adimlardan bazilarinin default degerleri dolayisiyla ayarlanmasina ihtiyaÃ§ yoktur.
+	eger portun basit bir digital fonksiyon Ã¶zelligi kullanilmak istenirse asasgidaki adimlarin takip edilmesi yeterlidir.
 	*Clock ayarlanmalidir.
 	*DIR registerinin ayarlanmalidir
 	*EN(digital enable) registerinin ayarlanmalidir
 	*/
-/*															 Portun base adresi			RCGCGPIO registerinin offseti				RCGCGPIO'nun adres degeri
-																												Yani clock registiri
+/*				      Portun base adresi			RCGCGPIO registerinin offseti				RCGCGPIO'nun adres degeri
+										Yani clock registiri
 	Physical address of RCGCGPIO = 0x400FE000 			+ 		0x608        =                      0x400FE608
-*/
+        RCGCGPIO'un fiziksel adresi 0x400FE000*/
 # define SYSCTL_RCGCGPIO_R (*(( volatile unsigned long *)0x400FE608)) // pointer mantigi ile clock registestirinin adresine erisim
 
 /*
@@ -46,7 +46,7 @@
 # define GPIO_PORTF_DEN_R (*(( volatile unsigned long *)0x4002551C))
 
 /* Son olarak hersey ayarlanmis durumda ama bir sorun var biz datayi nereden okuyacagiz yada nereye yazacagiz
-	 Bunun için portun DATA registirini kullanacagiz
+	 Bunun iÃ§in portun DATA registirini kullanacagiz
 */
 
 /*
@@ -68,9 +68,9 @@ GPIO_PORTF_DIR_R  = 0x0E ;  // Configure ORTF Pin1, 2 and 3 digital output pins
 }
 
 /*
-PF1 – LED red
-PF2 – LED blue
-PF3 – LED green
+PF1 â€“ LED red
+PF2 â€“ LED blue
+PF3 â€“ LED green
 DIR registirinda 1 degerini verdigimiz pinler output olurlar
 
 GPIO_PORTF_DATA_R |= 0x04; // mavi LED
@@ -79,7 +79,7 @@ GPIO_PORTF_DATA_R |= 0x08; // yesil LED
 
 /*
 	Ayrica asagidaki header file dosyasini kodumuza include ederek yani dahil ediyoruz register adreslerini 
-	Bir mnemonic degiskenle adlandirma isinden kurtulabiliriz görüldügü gibi çok daha sade bir kod
+	Bir mnemonic degiskenle adlandirma isinden kurtulabiliriz gÃ¶rÃ¼ldÃ¼gÃ¼ gibi Ã§ok daha sade bir kod
 */
 
 /*
